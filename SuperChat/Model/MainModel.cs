e018489d75a64d48be1e0b07d0b8ce6ca17623f3
a <a>
2022-05-05 16:48:49 +0900

    コミット

---
+++ SuperChat/Model/MainModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperChat.Model
+{
+    public class MenuItems
+    {
+        public string MenuName { get; set; }
+        public string MenuImage { get; set; }
+    }
+
+    public class HomeItems
+    {
+        public string HomeName { get; set; }
+        public string HomeImage { get; set; }
+    }
+
+    public class DesktopItems
+    {
+        public string DesktopName { get; set; }
+        public string DesktopImage { get; set; }
+    }
+}
---
+++ SuperChat/ViewModel/NavigationViweModel.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Input;
+using SuperChat.Model;
+
+namespace SuperChat.ViewModel
+{
+    class NavigationViweModel : INotifyPropertyChanged
+    {
+        private CollectionViewSource MenuItemsCollection;
+
+        public ICollectionView SourceCollection => MenuItemsCollection.View;
+
+        public NavigationViweModel()
+        {
+            ObservableCollection<MenuItems> menuItems = new ObservableCollection<MenuItems>
+            {
+                new MenuItems { MenuName = "Home", MenuImage = @"Assets/Home_Icon.png" },
+                new MenuItems { MenuName = "Desktop", MenuImage = @"Assets/Desktop_Icon.png" },
+            };
+
+            MenuItemsCollection = new CollectionViewSource { Source = menuItems };
+            MenuItemsCollection.Filter += MenuItems_Filter;
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        // Text Search Filter.
+        private string filterText;
+        public string FilterText
+        {
+            get => filterText;
+            set
+            {
+                filterText = value;
+                MenuItemsCollection.View.Refresh();
+                OnPropertyChanged("FilterText");
+            }
+        }
+
+        private void MenuItems_Filter(object sender, FilterEventArgs e)
+        {
+            if (string.IsNullOrEmpty(FilterText))
+            {
+                e.Accepted = true;
+                return;
+            }
+
+            MenuItems _item = e.Item as MenuItems;
+            if (_item.MenuName.ToUpper().Contains(FilterText.ToUpper()))
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
+        }
+
+        // Select ViewModel
+        private object _selectedViewModel;
+        public object SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set { _selectedViewModel = value; OnPropertyChanged("SelectedViewModel"); }
+        }
+
+        // Switch Views
+        public void SwitchViews(object parameter)
+        {
+            switch (parameter)
+            {
+                case "Home":
+                    //SelectedViewModel = new HomeViewModel();
+                    break;
+                case "Desktop":
+                    SelectedViewModel = new DesktopViewModel();
+                    break;
+            }
+        }
+
+        // Menu Button Command
+        private ICommand _menucommand;
+        public ICommand MenuCommand
+        {
+            get
+            {
+                if (_menucommand == null)
+                {
+                    _menucommand = new RelayCommand(param => SwitchViews(param));
+                }
+                return _menucommand;
+            }
+        }
+    }
+}
